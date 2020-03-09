7ffba4288edb42d1ee35717d4de653c2e414a709
zhanglei <zhanglei@ctvit.com.cn>
2020-03-09 18:26:29 +0800

    修改

--- app/src/main/java/com/ctvit/mylibtest/MainActivity.java
+++ app/src/main/java/com/ctvit/mylibtest/MainActivity.java
@@ -3,7 +3,6 @@ package com.ctvit.mylibtest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
 import android.widget.TextView;
 
 import com.ctvit.mylibrary.Name;
